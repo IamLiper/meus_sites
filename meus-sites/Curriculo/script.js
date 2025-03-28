4e57c870328060b5e043fb202d5815f6551ea9b8
Luís Felipe Silva Ferreira <152612333+IamLiper@users.noreply.github.com>
2025-03-28 12:54:29 -0300

    curriculo online

---
+++ meus-sites/Curriculo/script.js
@@ -0,0 +1,7 @@
+document.getElementById("githubButton").addEventListener("click", function() {
+    window.open("https://github.com/IamLiper", "_blank");
+});
+
+document.getElementById("sitesButton").addEventListener("click", function() {
+    window.open("https://github.com/IamLiper/meus_sites", "_blank");
+});
