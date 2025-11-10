8beb49107eee28d4e06ab2d057bb594c64a8ce3d
Luís Felipe Silva Ferreira <152612333+IamLiper@users.noreply.github.com>
2025-11-10 11:34:53 -0300

    atualizando o curriculo

--- meus-sites/Curriculo/script.js
+++ meus-sites/Curriculo/script.js
@@ -7,6 +7,5 @@ document.getElementById("sitesButton").addEventListener("click", function() {
 });
 
 document.getElementById("whatsapp").addEventListener("click", function() {
-    window.open("https://api.whatsapp.com/send?phone=5571996549388&text=Ol%C3%A1%2C%20vi%20seu%20site%20e%20quero%20saber%20mais!", target="_blank");
-})
-
+    window.open("https://api.whatsapp.com/send?phone=5571996549388&text=Ol%C3%A1%2C%20vi%20seu%20site%20e%20quero%20saber%20mais!", "_blank");
+});
