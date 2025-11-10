eb903859a1eca997a09db3a36d0af7a2f5c8be3e
Luís Felipe Silva Ferreira <152612333+IamLiper@users.noreply.github.com>
2025-11-10 11:00:59 -0300

    Update script.js

--- meus-sites/Curriculo/script.js
+++ meus-sites/Curriculo/script.js
@@ -5,3 +5,7 @@ document.getElementById("githubButton").addEventListener("click", function() {
 document.getElementById("sitesButton").addEventListener("click", function() {
     window.open("https://github.com/IamLiper/meus_sites", "_blank");
 });
+
+document.getElementById("whatsapp").addEventListener("click", function() {
+    window.open("wa.me/5571996549388");
+})
